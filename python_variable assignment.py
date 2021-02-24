3e2628a67aadce3733be87e08f86a619120cac5b
Purnima Gupta <purnimagupta@Purnimas-Air.home>
2021-02-24 12:25:36 +0200

    variable assignment

---
+++ python_variable assignment.py
@@ -0,0 +1,11 @@
+print("assigning the a variable with value "+str(5))
+a=5
+print("now the value changes to"+str(a))
+print(a)
+print("assigning the a variable with value 5")
+a=10
+print(a)
+# we can assign the same variable with different name
+a=12
+print("assigning the same variable with diff name which is " + str(a))
+# print(a)
