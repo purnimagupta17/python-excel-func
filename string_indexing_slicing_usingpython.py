be59f98b5a651f9ab08bef5f82ba69047a40a27d
Purnima Gupta <purnimagupta@Purnimas-Air.home>
2021-02-24 23:24:43 +0200

    string practice

---
+++ string_indexing_slicing_usingpython.py
@@ -0,0 +1,13 @@
+print('Hello World')
+
+#to shift the 'World' to next line, we can use \n in the symtax as follows:
+print('Hello \nWorld')
+
+#to count the number of characters in a string, we use len function as follows:
+str1="Hello"
+print(len(str1))
+
+#to assign the index number to a string, for example:
+ss="Hello World"
+print(ss[0])
+print(ss[3:8])
