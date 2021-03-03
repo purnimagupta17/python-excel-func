1619231a9118cb5273e82f8dfc26aaf9a1c836b9
Purnima Gupta <purnimagupta@Purnimas-Air.home>
2021-03-03 13:42:13 +0200

    adding new file

--- string_indexing_slicing_usingpython.py
+++ string_indexing_slicing_usingpython.py
@@ -11,3 +11,20 @@ print(len(str1))
 ss="Hello World"
 print(ss[0])
 print(ss[3:8])
+
+#to jump on the string:
+str2="abcdefghijk"
+print(str2[::])
+
+#to exclude the 2nd character from string
+print(str2[::2])
+
+#to reverse the string
+print(str2[::-1])
+
+#to edit the name in a string
+
+name='sam'
+#name[0]='P'
+last_letters=name[1:]
+last-letters
